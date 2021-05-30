94919ac0aef4d11aa350518e092c21a64e9813cd
phosseini <pdr.hosseini@gmail.com>
2021-05-30 00:41:15 -0400

    changing field name in copa2bert

--- crest/utils.py
+++ crest/utils.py
@@ -491,11 +491,11 @@ def copa2bert(folder_path, split='dev'):
             span2_incorrect = spans[2] if answer == 1 else spans[1]
 
             if item.attrib["asks-for"] == "cause":
-                data.append({'sequence': '[CLS] {} [SEP] {} [SEP]'.format(span2_correct, span1_premise), 'label': 1})
-                data.append({'sequence': '[CLS] {} [SEP] {} [SEP]'.format(span2_incorrect, span1_premise), 'label': 0})
+                data.append({'text': '[CLS] {} [SEP] {} [SEP]'.format(span2_correct, span1_premise), 'label': 1})
+                data.append({'text': '[CLS] {} [SEP] {} [SEP]'.format(span2_incorrect, span1_premise), 'label': 0})
             elif item.attrib["asks-for"] == "effect":
-                data.append({'sequence': '[CLS] {} [SEP] {} [SEP]'.format(span1_premise, span2_correct), 'label': 1})
-                data.append({'sequence': '[CLS] {} [SEP] {} [SEP]'.format(span1_premise, span2_incorrect), 'label': 0})
+                data.append({'text': '[CLS] {} [SEP] {} [SEP]'.format(span1_premise, span2_correct), 'label': 1})
+                data.append({'text': '[CLS] {} [SEP] {} [SEP]'.format(span1_premise, span2_incorrect), 'label': 0})
 
     except Exception as e:
         print("[crest-log] COPA. Detail: {}".format(e))
